886bdd1be8d879984edf5b4dcb0561e580845f1a
David Kolia <Dkolia@icloud.com>
2019-04-20 15:25:50 -0500

    Streamlined File Structure, Made H1 Appear

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component
+{
+    render(){
+        return (<h1>Hello World2</h1>)
+    }
+}
+
+// class Hero extends React.Component
+// {
+//     append to dom element
+// }
+
+// class Navbar extends React.Component
+// {
+//     Append to dom element
+// }
+
+
+export default App;
